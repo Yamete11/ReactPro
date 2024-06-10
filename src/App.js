0e8de69e1dc89ab1d1e3d40d1eb4b01053fcb906
Gleb Ivanov <s22223@pjwstk.edu.pl>
2024-06-10 22:51:06 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useRef, useState} from 'react'
+import Counter from "./components/Counter";
+import './styles/App.css'
+import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
 
 function App() {
+    const [posts, setPosts] = useState([
+        {id:1, title: 'JavaScript', body: 'Description'},
+        {id:2, title: 'JavaScript', body: 'Description'},
+        {id:3, title: 'JavaScript', body: 'Description'}
+    ])
+
+    const [post, setPost] = useState({title: '', body: ''})
+
+
+
+    const addNewPost = (e) => {
+        e.preventDefault()
+        setPosts([...posts, {...post, id: Date.now()}])
+        setPost({title: '', body: ''})
+
+
+    }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <form>
+            <MyInput
+                value={post.title}
+                onChange={e => setPost({...post, title: e.target.value})}
+                type="text"
+                placeholder="title"
+            />
+            <MyInput
+                value={post.body}
+                onChange={e => setPost({...post, body: e.target.value})}
+                type="text"
+                placeholder="description"
+            />
+            <MyButton onClick={addNewPost}>Create post</MyButton>
+        </form>
+        <PostList posts={posts} title={'List about js'}></PostList>
     </div>
   );
 }
